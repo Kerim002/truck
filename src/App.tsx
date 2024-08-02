6dee0fea5aa0d541d21566d818db3bd8046deee3
DovranA <atadov23@gmail.com>
2024-08-02 10:12:21 +0500

    dployed

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import RouterWithFallback from "./app/router/RouterWithFallback";
+
+function App() {
+  return <RouterWithFallback />;
+}
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import RouterWithFallback from "./app/router/RouterWithFallback.tsx";
 import { Provider } from "react-redux";
 import { store } from "./app/store/store.ts";
+import App from "./App.tsx";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <RouterWithFallback />;
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
